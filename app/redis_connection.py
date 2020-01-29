46f857f7b2abd2dc9fbf4d733752139845ba8b14
Claus Gerull <cgerull@gmail.com>
2020-01-29 18:28:50 +0100

    Fix Redis connection.

--- app/redis_connection.py
+++ app/redis_connection.py
@@ -4,7 +4,8 @@ from app import app
 def get_redis():
     r = None
     if app.config["REDIS_URL"]:
-        r = redis.from_url(os.environ.get("REDIS_URL"))
+        print("Connect to Redis at {}".format(app.config["REDIS_URL"]))
+        r = redis.from_url(app.config["REDIS_URL"])
     return r
 
 def increment_redis_counter(r, counter = 'counter'):
